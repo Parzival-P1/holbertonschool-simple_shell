0e015f07d30c927a8548ab117f62d4545f1aa65e
Paco Hdez <francj.hdez@gmail.com>
2022-12-08 14:47:51 -0600

    fixing minor bugs

--- shell.h
+++ shell.h
@@ -18,7 +18,7 @@ int countBySpace(char comando[]);
 int countPipeOcurrence(char comando[]);
 int countFluxOcurrence(char comando[]);
 void split(char comando[], char *slicedCommand[], char c[]);
-void finalCommandTokens(char * finalTokens[], char comando[]);
+void finalCommandTokens(char *finalTokens[], char comando[]);
 void executeBasicCommand(char *tokens[], char comando[]);
 void executeCommandWithPipes(char *args1[], char *args2[]);
 void executeOutputFileCommand(char args[], char fileName[]);
