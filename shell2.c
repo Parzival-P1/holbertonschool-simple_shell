771810b553f9a1c1f0cf81c2ddfd1564e6429a8d
Paco Hdez <francj.hdez@gmail.com>
2022-12-08 13:59:47 -0600

    fixing minor bugs

--- shell2.c
+++ shell2.c
@@ -1,43 +1,72 @@
 #include "shell.h"
+/**
+ * countBySpace - count the num of tokens in the command line
+ * @comando: gets the command enter by the user
+ * Return: the number of charachters in the command line
+ */
 
-/* Cuenta la cantidad de tokens en el comando (cuenta un token cuando encuentra un espacio) */
-int countBySpace(char comando[]){
-  char * puntero;
-  int cont = 0;
-  puntero = strtok(comando, " ");
+int countBySpace(char comando[])
+{
+	char *puntero;
 
-  while(puntero != NULL){
-    cont++;
-    puntero = strtok(NULL, " ");
-  }
-  return cont;
+	int cont = 0;
+
+	puntero = strtok(comando, " ");
+	while (puntero != NULL)
+	{
+		cont++;
+		puntero = strtok(NULL, " ");
+	}
+	return (cont);
 }
 
-/* Cuenta si hay un pipe en el comando */
-int countPipeOcurrence(char comando[]){
-  if(strchr(comando, '|')!=NULL){ 
-    return 1;
-  }
-  return 0;
+/**
+ * countPipeOcurrence - count if there's a pipe in the command line
+ * @comando: gets the command enter by the user
+ * Return: Always 0 (success)
+ */
+int countPipeOcurrence(char comando[])
+{
+	if (strchr(comando, '|') != NULL)
+	{
+		return (1);
+	}
+	return (0);
 }
 
-/* Cuenta si hay un '>' en el comando */
-int countFluxOcurrence(char comando[]){
-  if(strchr(comando, '>')!=NULL){ 
-    return 1;
-  }
-  return 0;
+/**
+ * countFluxOcurrence - counts if there's an '>' in the command line
+ * @comando: gets the command enter by the user
+ * Return: Always 0 (success)
+ */
+int countFluxOcurrence(char comando[])
+{
+	if (strchr(comando, '>') != NULL)
+	{
+		return (1);
+	}
+	return (0);
 }
 
-/* Separa el comando en dos subarreglos cuando encuentra el pipe o el '>' */
-void split(char comando[], char *slicedCommand[], char c[]){
-  char * puntero; 
-  int i = 0;
-  puntero = strtok(comando, c);
-  
-  while(puntero != NULL){
-    slicedCommand[i] = puntero;
-    puntero = strtok(NULL, c);
-    i++;
-  } 
+/**
+ * split - splits the command in 2 subarrays if finds the pipe
+ * or the '>' simbol
+ * @comando: gets the command enter by the user
+ * @slicedCommand: slice the arr
+ * @c: gets the char enter by the user
+ */
+
+void split(char comando[], char *slicedCommand[], char c[])
+{
+	char *puntero;
+	int i = 0;
+
+	puntero = strtok(comando, c);
+	while (puntero != NULL)
+	{
+		slicedCommand[i] = puntero;
+		puntero = strtok(NULL, c);
+		i++;
+	}
 }
+
